a55640a94342f8e265a1c7eb7c655c3134357201
lawrence <lodhiambo26@gmail.com>
2024-01-18 08:20:38 +0000

    task 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,7 @@
--- This script creates a table 'users' with specified attributes and requirements
-
-
-IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')
-BEGIN
-	CREATE TABLE users (
-		id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-		email VARCHAR(255) UNIQUE NOT NULL,
-		name VARCHAR(255)
-		);
-END;
+-- SQL script that creates a table users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
