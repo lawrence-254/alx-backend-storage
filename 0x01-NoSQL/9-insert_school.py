cccd508668916f047045d5cd58b5f0860515a400
lawrence <lodhiambo26@gmail.com>
2024-01-22 22:50:18 -0800

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+a Python function that inserts a new document in a collection
+based on kwargs:
+'''
+def insert_school(mongo_collection, **kwargs):
+    '''
+    mongo_collection will be the pymongo collection object
+    Returns the new _id
+    '''
+    new_doc = mong0_collection.insert_one(kwargs)
+    return new_doc.inserted_id
