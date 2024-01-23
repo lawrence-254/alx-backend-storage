6b3f3fb60cab63abca738a9e830e48861bbd46eb
lawrence <lodhiambo26@gmail.com>
2024-01-22 22:40:16 -0800

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+a Python function that lists all documents in a collection
+'''
+
+
+def list_all(mongo_collection):
+    '''
+    Return an empty list if no document in the collection
+    mongo_collection will be the pymongo collection object
+    '''
+    return (list(mongo_collection.find()))
