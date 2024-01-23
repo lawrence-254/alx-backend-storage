8beee5c0ef238a2360bb27bf6b70b3e3d2393309
lawrence <lodhiambo26@gmail.com>
2024-01-23 13:13:13 -0800

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,8 +6,35 @@ MongoDB
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection):
+def log_stats(nginx_collection):
     '''
     Database: logs
     Collection: nginx
-    Display (same as the example):Display (i
+    Display (same as the example):
+    first line: x logs where x is the number of documents in this
+    collection second line: Methods:    5 lines with the number of documents with the method = [    "GET", "POST", "PUT", "PATCH", "DELETE"] in this order (
+    see example below - warning: it’s a tabulation before each line)
+    one line with the number of documents with:
+    method=GET
+    path=/status
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+        ))
+    print('{} status check'.format(status_checks_count))
+
+def exe():
+    '''
+    executes nginx with params
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_stats(client.logs.nginx)
+
+if __name__ == "__main__":
+    exe()
